bc3888f5ef6d3e9741edcac70c447761aabb62bd
Solana Dev10 <solanadeveloper10@gmail.com>
2025-02-03 18:17:48 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <iframe
+        src="https://tweaks-nine.vercel.app/"
+        title="Deployed old website."
+        className="iframe"
+      />
+
+      <header className="header">
+        <a className="header-link" href="#">X</a>
+        <a className="header-link" href="#">Telegram</a>
+        <a className="header-link" href="#">Dexscreener</a>
+        <a className="header-link" href="#">Dextools</a>
+        <a className="header-link" href="#">Raydium</a>
+      </header>
+    </div>
+  )
+}
+
+export default App
