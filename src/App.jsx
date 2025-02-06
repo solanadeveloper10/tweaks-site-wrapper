9d81d0af32fbb19b2163bd9cde34eb3e870030f3
deadtowel <properfraction66@yahoo.com>
2025-02-06 13:33:34 +0200

    add mobile header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 function App() {
   return (
     <div className="wrapper">
+      <div className="loading">
+        Loading...
+      </div>
+
       <iframe
         src="https://tweaks-nine.vercel.app/"
         title="Deployed old website."
@@ -10,11 +14,31 @@ function App() {
       />
 
       <header className="header">
-        <a className="header-link" href="#">X</a>
-        <a className="header-link" href="#">Telegram</a>
-        <a className="header-link" href="#">Dexscreener</a>
-        <a className="header-link" href="#">Dextools</a>
-        <a className="header-link" href="#">Raydium</a>
+        <a className="header-link" href="#">
+          <span className="link-text">X</span>
+
+          <img className="link-img" src="/icons/x-twitter.png" alt="X icon." />
+        </a>
+        <a className="header-link" href="#">
+          <span className="link-text">Telegram</span>
+
+          <img className="link-img" src="/icons/telegram.png" alt="Telegram icon." />
+        </a>
+        <a className="header-link" href="#">
+          <span className="link-text">Dexscreener</span>
+
+          <img className="link-img" src="/public/icons/dexscreener.png" alt="Dexscreener icon." />
+        </a>
+        <a className="header-link" href="#">
+          <span className="link-text">Dextools</span>
+
+          <img className="link-img" src="/public/icons/dextools_icon.png" alt="Dextools icon." />
+        </a>
+        <a className="header-link" href="#">
+          <span className="link-text">Raydium</span>
+
+          <img className="link-img" src="/public/icons/raydium_icon.png" alt="Raydium icon." />
+        </a>
       </header>
     </div>
   )
